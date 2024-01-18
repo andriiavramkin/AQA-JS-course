ff15e26679661c8854b169557fcdc233f7e18048
Andrii Avramkin <avramkin.ad@gmail.com>
2024-01-18 20:20:10 +0100

    lesson 3 notes and files

---
+++ intro_js/index.js
@@ -0,0 +1,38 @@
+
+/*
+// my comment
+const name = "Andrii" 
+
+console.log("Hello world!")
+console.log('Hello world!'.length)
+
+console.log("Hello 'Max' world!")
+console.log(`May name is ${name}`)
+
+
+console.log('May name is ${name}')
+
+
+//bigInt
+const num = 1n
+
+console.log(num + 1)
+*/
+
+//boolean
+const isMarried = true
+const isGoodWeather = false
+
+//undefined
+const pet = undefined
+let age 
+
+console.log("name", pet)
+console.log('age', age)
+
+const firstName = null
+
+let surname
+const nickname = undefined
+
+
