31c84c7028b078f0a59dd1db34144beb58115b0d
Andrii Avramkin <avramkin.ad@gmail.com>
2024-02-02 14:25:25 +0100

    fix to a commit - 7824702c6057cad4883963472d447563b7f68623

--- hw-7/task1.js
+++ hw-7/task1.js
@@ -1,13 +1,13 @@
 
-function handleNum (number, fun1, fun2) {
+function handleNum (number, onEven, onOdd) {
     if (number % 2 === 0) {
-        fun1()
+        onEven()
     } else {
-        fun2()
+        onOdd()
     } 
 }
 
 const handleEven = () => console.log('number is even')
 const handleOdd = () => console.log('number is odd')
 
-handleNum(15, handleEven, handleOdd)
+handleNum(16, handleEven, handleOdd)
