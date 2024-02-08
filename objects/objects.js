a8d68e04f318254938f2b3ab4b7282126a942c01
Andrii Avramkin <avramkin.ad@gmail.com>
2024-02-08 22:05:56 +0100

    lecture -9

---
+++ objects/objects.js
@@ -0,0 +1,41 @@
+//example 1
+// const user = {
+//     name: 'Alice',
+//     age: 25,
+
+// }
+
+// console.log(user)
+// console.log(user.name)
+// console.log(user.age)
+
+// user.age = user.age + 1
+// console.log(user.age)
+
+
+
+
+// example 2
+
+// const user = {
+//         name: 'Alice',
+//         age: 25,
+//         // sayHello: function(){
+
+//         // }
+//         sayHello(targetPersonName){
+//             console.log(`Hi ${targetPersonName}! I am ${this.name}`)
+//         }
+//     }
+
+//     user.sayHello('Joe')
+
+
+//     delete user.age
+//     console.log(user.age)
+//     console.log(user)
+
+
+
+
+// example 3
