ea79de481b0af1a1cb117719f8d9084787003250
Andrii Avramkin <avramkin.ad@gmail.com>
2024-02-16 00:05:07 +0100

    hw-10 is ready for review

---
+++ homeworks/hw-10/Book.js
@@ -0,0 +1,75 @@
+
+export default class Book {
+    constructor (title, author, year) {
+        this._title = title
+        this._author = author
+        this._year = year
+    }
+
+
+    // task3 getter-setter
+    get title() {
+        return this._title
+    }
+
+    set title(value) {
+        if (typeof value !== 'string') {
+            throw new Error('Error: Book\'s title should be in string type')
+        } 
+
+        if (value.length < 3) {
+            throw new Error(' Title length should be at least 3 characters')
+        }
+
+        this._title = value
+    }
+
+    get author() {
+        return this._author
+    }
+
+    set author(value) {
+        if (typeof value === 'string') {
+            throw new Error('Error: Author\'s name should be in string type')
+        }
+
+        if (value.length < 1) {
+            throw new Error(' Author\'s name should be at least 1 character')
+        }
+
+        this._author = value
+    }
+
+    get year() {
+        return this._year
+    }
+
+    set year(value) {
+        if (typeof value !== 'number' || isNaN(value)) {
+            throw new Error('Error: Publishing year should be in number type')
+        } 
+            
+        if (value.length !== 4) {
+            throw new Error('Publishing year should be strictly 4 digits')
+        }
+
+        this._year = value
+    }
+
+    //task4 
+    static findOldestBook (books) {
+        let oldestBook = books[0]
+
+        for (let i = 1; i < books.length; i++) {
+            if (books[i].year < oldestBook.year) {
+                oldestBook = books[i];
+            }
+        }
+        return oldestBook;
+    }
+
+
+    printInfo () {
+        console.log(`Your book is ${this.title} written by ${this.author} at ${this.year}.`)
+    }
+}
---
+++ homeworks/hw-10/task1.js
@@ -0,0 +1,67 @@
+import Book from "./Book.js"
+import EBook from "./EBook.js"
+
+//task 1
+console.log('task 1')
+// valid
+const book1 = new Book('Harry Potter', 'J.K.Rowling', 1994)
+book1.printInfo()
+
+const book2 = new Book('Lord of the Rings', 'J.R.R.Tolkien', 1954)
+book2.printInfo()
+
+const book3 = new Book('The Hobbit', 'J.R.R.Tolkien', 1937)
+book3.printInfo()
+
+console.log('-------------------------------------------')
+console.log(' ')
+
+//invalid
+console.log('task 3')
+const book4 = new Book(true, 'J.K.Rowling - # ', 1994)
+book4.printInfo()
+
+const book5 = new Book('Lord of the Rings #', 'J.R.R.Tolkien #', 'peter')
+book5.printInfo()
+
+console.log('-------------------------------------------')
+console.log(' ')
+
+//task 2
+console.log('task 2')
+//valid
+const eBook1 = new EBook('The Great Gatsby', 'F. Scott Fitzgerald', 1925, 'pdf')
+eBook1.printInfo()
+
+const eBook2 = new EBook('The Theory of (Not Quite) Everything', 'Kara Gnodde', 2023, 'mb2')
+eBook2.printInfo()
+
+console.log('-------------------------------------------')
+console.log(' ')
+
+//inValid
+console.log('task 3')
+const eBook3 = new EBook('The Great Gatsby #', 'F. Scott Fitzgerald #', 1925, 45)
+eBook3.printInfo()
+
+const eBook4 = new EBook('The Theory of (Not Quite) Everything #', 'Kara Gnodde #', 2023, 'mb22334')
+eBook4.printInfo()
+
+console.log('-------------------------------------------')
+console.log(' ')
+
+//task 4
+console.log('task 4')
+const books = [book1, book2, book3, eBook1, eBook2]
+console.log("The oldest book is ", Book.findOldestBook(books));
+
+console.log('-------------------------------------------')
+console.log(' ')
+
+//task 5
+console.log('task 5')
+const eBook = EBook.fromBookAndFileFormat(book1, 'pdf')
+console.log("eBook:", eBook);
+
+console.log('-------------------------------------------')
+console.log(' ')
